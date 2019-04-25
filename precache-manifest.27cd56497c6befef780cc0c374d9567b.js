778be7dd476a02bb39ae8e1dc261190c3343ecbf
Przemyslaw Janowski <Przemyslaw_Janowski@epam.com>
2019-04-25 10:50:39 +0200

    Updates

---
+++ precache-manifest.27cd56497c6befef780cc0c374d9567b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2fead1842b8288d49de",
+    "url": "/static/css/main.861d17de.chunk.css"
+  },
+  {
+    "revision": "b2fead1842b8288d49de",
+    "url": "/static/js/main.94d5cf81.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fc1a04f5442fc70ce71f",
+    "url": "/static/js/2.f9075ab4.chunk.js"
+  },
+  {
+    "revision": "5b4d88ebae45b716d5b56aec1921be6f",
+    "url": "/index.html"
+  }
+];
