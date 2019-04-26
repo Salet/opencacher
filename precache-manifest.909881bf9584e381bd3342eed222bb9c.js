6f8a09beafa4d15c66a4513cb0c7b9eb74632586
Przemyslaw Janowski <Przemyslaw_Janowski@epam.com>
2019-04-26 17:06:50 +0200

    Updates

---
+++ precache-manifest.909881bf9584e381bd3342eed222bb9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f10dc87c0dd91cf01e10",
+    "url": "/opencacher/static/css/main.916af188.chunk.css"
+  },
+  {
+    "revision": "f10dc87c0dd91cf01e10",
+    "url": "/opencacher/static/js/main.1e7c10f5.chunk.js"
+  },
+  {
+    "revision": "3c6c093335a3cbd031c3",
+    "url": "/opencacher/static/js/runtime~main.e4d53384.js"
+  },
+  {
+    "revision": "08e6dcb1f224a455d76e",
+    "url": "/opencacher/static/js/2.1fccb98a.chunk.js"
+  },
+  {
+    "revision": "486e73318261d6fec4b7ae69609bc8fb",
+    "url": "/opencacher/index.html"
+  }
+];
